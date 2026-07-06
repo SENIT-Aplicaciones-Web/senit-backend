882b2498ec3769c5cde73243795280f0a8fe1b6e
Leonardo Rafael Garcia Villanueva <leonardo_351461@hotmail.com>
2026-07-06 02:51:14 -0500

    fix: password min length

--- Senit.Platform.API/Iam/Interfaces/Rest/Resources/CreateUserResource.cs
+++ Senit.Platform.API/Iam/Interfaces/Rest/Resources/CreateUserResource.cs
@@ -30,7 +30,7 @@ public class CreateUserResource
     public string Email { get; init; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required")]
-    [MinLength(6, ErrorMessage = "Password must have at least 6 characters")]
+    [MinLength(4, ErrorMessage = "Password must have at least 4 characters")]
     [StringLength(250, ErrorMessage = "Password must not exceed 250 characters")]
     [OpenApiExample("Recepcion123")]
     public string Password { get; init; } = string.Empty;
